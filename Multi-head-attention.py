5400b231500bfd1a18c0a11400272ab6f1938d9d
Priyam <pd333a3@gmail.com>
2026-05-27 20:47:39 +0530

    File  modified

--- Multi-head-attention.py
+++ Multi-head-attention.py
@@ -2,3 +2,25 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import math
+
+class MHA(nn.Module):
+
+    def __init__(self,d_model,n_heads):
+        super.__init__()
+        self.d_head=n_heads//d_model
+        self.n_heads=n_heads
+        self.heads=nn.ModuleList([
+            SelfAttention(d_model,self.d_head)
+            for _ in range(n_heads)
+        ])
+
+    def forward(self,x, return_attention=False):
+        if return_attention:
+            outputs,attentions=[],[]
+            for head in self.heads:
+                output,attention= head(x,return_attention=True)
+                outputs.append(output)
+                attentions.append(attention)
+                return sum(outputs),torch.stack(attentions,dim=0)
+        else:
+            return sum(head(x) for head in self.heads)
